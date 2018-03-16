8bf8f0962b2d91fe1e112b833e1b0e9226135a84
yennanliue <f339339@gmail.com>
2018-03-16 07:45:50 +0000

    add for loop and decision logic demo

---
+++ forloop_demo.java
@@ -0,0 +1,30 @@
+class forloop_demo {
+
+    public static void main(String args[]) {
+        for (int i = 1; i < 10; i++) {
+        System.out.println("loop :" + i );
+        if(i > 5){System.out.println("i is big, where i = " + i); }
+        else{System.out.println("i is small, where i = " + i); }
+
+        }
+
+    }
+
+}
+
+
+
+/*
+
+class forloop_demo {
+
+    public static void main(String args[]) {
+        for (int i = 1; i < 10; i++) {
+
+            System.out.println("the value of i is " + i);
+        }
+
+    }
+
+}
+*/
