43dc11a0486deee4641611c90090e1012bc6a987
yennanliue <f339339@gmail.com>
2018-04-02 16:55:17 +0100

    add java sqlite simple demo

--- SQLiteJDBC.java
+++ SQLiteJDBC.java
@@ -7,6 +7,19 @@ http://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/
 */
 
 
+/*
+# download SQLite JDBC
+# https://bitbucket.org/xerial/sqlite-jdbc/downloads/
+# move downloaded SQLite JDBC to the java route (or to current route and run with "-classpath" command)
+# http://jashliao.pixnet.net/blog/post/222225249-sqlite-%E2%80%93%C2%A0java
+# compile 
+$ javac SQLiteJDBC.java
+# run the java script
+$ java -classpath ".:sqlite-jdbc-3.21.0.jar" SQLiteJDBC
+
+*/
+
+
 import java.sql.*;
 
 public class SQLiteJDBC {
