5c749e4b50030bca7f260aeddf678e21246c2f53
yennanliue <f339339@gmail.com>
2018-04-15 13:40:53 +0100

    add java method and method call demo code

---
+++ method_demo.java
@@ -0,0 +1,40 @@
+public class method_demo
+{
+public static void main(String[] args)
+{
+	int integer = 100; 
+	square(integer);
+	print_(integer);
+	plus(integer);
+}
+	// ---------------- method 
+
+	public static int square (int input)
+
+	{
+	System.out.println(input*input ) ; 
+	return input*input ; 
+
+	}
+
+
+	public static int print_ (int input)
+
+	{
+	System.out.println(input) ; 
+	return input; 
+
+	}
+
+	public static int plus (int input)
+
+	{
+	System.out.println(input+9) ; 
+	return input+9; 
+
+	}
+
+
+	// ---------------- method 
+
+}
