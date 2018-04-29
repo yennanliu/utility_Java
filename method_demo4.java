8e5dd45aafbc2294bd5bfbff12ce20731aeb64ff
yennanliue <f339339@gmail.com>
2018-04-29 16:05:12 +0100

    add method delete csv

---
+++ method_demo4.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.sql.*;
+
+
+
+// credit : https://gist.github.com/madan712/8683348
+
+public class method_demo4 {
+
+  public static final String delimiter = ",";
+  public static void main(String[] args) {
+  // step 1 : csv operation
+  String csvFile = "/Users/yennanliu/utility_java/df_test.csv";
+  CSVReader.read(csvFile);
+  // step 2 : integer operation 
+  int integer = 100; 
+  square(integer);
+  // step 3 sqlite 
+  String arg = "some arg";
+  parse_sqlite(arg);
+  delete_csv(arg);
+  //create_csv(arg);
+}
+  
+  // ---------------- method 
+
+
+  public static int print_ (int input)
+
+  {
+  System.out.println(input) ; 
+  return input; 
+  }
+
+  public static int plus (int input)
+
+  {
+  System.out.println(input+9) ; 
+  return input+9; 
+
+  }
+
+
+  public static int square (int input)
+
+  {
+  System.out.println(input*input ) ; 
+  return input*input ; 
+
+  }
+
+  public static void delete_csv( String args )
+  {
+        File file = new File("/Users/yennanliu/utility_java/test.csv");
+         
+        if(file.delete())
+        {
+            System.out.println("File deleted successfully");
+        }
+        else
+        {
+            System.out.println("Failed to delete the file");
+        }
+    }
+
+
+  public static void read(String csvFile) {
+    try {
+      File file = new File(csvFile);
+      FileReader fr = new FileReader(file);
+      BufferedReader br = new BufferedReader(fr);
+
+      String line = "";
+
+      String[] tempArr;
+      while ((line = br.readLine()) != null) {
+        tempArr = line.split(delimiter);
+        for (String tempStr : tempArr) {
+          System.out.print(tempStr + " ");
+        }
+        System.out.println();
+      }
+      br.close();
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+
+  }
+
+
+  public static void parse_sqlite( String args )
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+      while ( rs.next() ) {
+         int id = rs.getInt("id");
+         String  name = rs.getString("name");
+         int age  = rs.getInt("age");
+         String  address = rs.getString("address");
+         float salary = rs.getFloat("salary");
+         System.out.println( "ID = " + id );
+         System.out.println( "NAME = " + name );
+         System.out.println( "AGE = " + age );
+         System.out.println( "ADDRESS = " + address );
+         System.out.println( "SALARY = " + salary );
+         System.out.println();
+      }
+      rs.close();
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Operation done successfully");
+  }
+
+
+  // ---------------- method 
+
+
+}
+
