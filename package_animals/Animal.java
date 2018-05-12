d874b569766db970b3445e8c49582bfeb80a5bfc
yennanliue <f339339@gmail.com>
2018-05-12 10:42:22 +0100

    add java package using demo

---
+++ package_animals/Animal.java
@@ -0,0 +1,12 @@
+/* File name : Animal.java */
+/* credit https://zh.wikibooks.org/wiki/Java/%E5%8C%85 */
+
+package package_animals;
+
+interface Animal {
+
+public void eat();
+
+public void travel();
+
+}
