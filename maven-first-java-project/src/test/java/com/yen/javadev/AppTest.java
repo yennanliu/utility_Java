67afecd8f2011615d054aad582575e23f6f72b3d
yennanliue <f339339@gmail.com>
2018-11-16 07:29:14 +0000

    fix imports

--- maven-first-java-project/src/test/java/com/yen/javadev/AppTest.java
+++ maven-first-java-project/src/test/java/com/yen/javadev/AppTest.java
@@ -4,9 +4,6 @@ package com.yen.javadev;
 import org.junit.Assert;
 import org.junit.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
