4f669087bce385a53e9c0ca56f47ac6a02283fec
yennanliue <f339339@gmail.com>
2018-11-19 15:41:38 +0000

    rename file

--- spark_learning/AirportsInUsa.java
+++ spark_learning/AirportsInUsa.java
@@ -5,7 +5,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-public class AirportsInUsaSolution {
+public class AirportsInUsa {
 
     public static void main(String[] args) throws Exception {
 
