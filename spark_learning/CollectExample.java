1367b105375e02fc33ae07714dbb72f148f5daf9
yennanliu <f339339@gmail.com>
2018-11-20 15:40:40 +0000

    backup dev code

---
+++ spark_learning/CollectExample.java
@@ -0,0 +1,42 @@
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectExample {
+
+    public static void main(String[] args) throws Exception {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        SparkConf conf = new SparkConf().setAppName("collect").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // collection 1 
+        List<String> inputWords = Arrays.asList("spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop");
+        JavaRDD<String> wordRdd = sc.parallelize(inputWords);
+        List<String> words = wordRdd.collect();
+
+        for (String  word : words) {
+            System.out.println(word);
+        }
+
+        // collection 2 
+        //JavaRDD<String> lines = sc.textFile("data/word_count.text"); 
+        //JavaRDD<String> words_collect =  lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator()).collect();
+
+        //for (String word_ : words_collect) {
+        //    System.out.print(word_);
+        //}
+    }
+}
+
+
+
+
+
+
+
