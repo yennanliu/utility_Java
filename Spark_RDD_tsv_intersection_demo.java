973fd71ee0b8e2f94812a848ed48379072f0a2b7
yennanliu <f339339@gmail.com>
2018-11-27 14:49:51 +0000

    rename filename in uniform way

--- Spark_RDD_tsv_intersection_demo.java
+++ Spark_RDD_tsv_intersection_demo.java
@@ -7,7 +7,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import java.util.Arrays;
 import java.util.List;
 
-public class Spark_tsv_RDD_intersection_demo {
+public class Spark_RDD_tsv_intersection_demo {
 
     public static void main(String[] args) throws Exception {
         Logger.getLogger("org").setLevel(Level.ERROR);
