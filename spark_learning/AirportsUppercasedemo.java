dc64f35c30815e5e92979f817f3ae2d1bbdf37c9
yennanliue <f339339@gmail.com>
2018-12-07 16:25:10 +0000

    add PairRDD mapValues demo

---
+++ spark_learning/AirportsUppercasedemo.java
@@ -0,0 +1,41 @@
+
+
+import commons.Utils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import scala.Tuple2;
+
+
+
+public class AirportsUppercasedemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Logger.getLogger("org").setLevel(Level.ERROR);
+
+        SparkConf conf = new SparkConf().setAppName("airports").setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airportsRDD = sc.textFile("data/airports.text");
+
+        JavaPairRDD<String, String> airportPairRDD = airportsRDD.mapToPair(getAirportNameAndCountryNamePair());
+
+        JavaPairRDD<String, String> upperCase = airportPairRDD.mapValues(countryName -> countryName.toUpperCase());
+
+        System.out.println("*** upperCase :\n" + upperCase.take(30) );
+
+        //upperCase.saveAsTextFile("out/airports_uppercase.text");
+        
+    }
+
+    private static PairFunction<String, String, String> getAirportNameAndCountryNamePair() {
+        return (PairFunction<String, String, String>) line -> new Tuple2<>(line.split(Utils.COMMA_DELIMITER)[1],
+                                                                           line.split(Utils.COMMA_DELIMITER)[3]);
+    }
+}
