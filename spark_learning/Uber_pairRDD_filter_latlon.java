bc6d928b1dc4be17a8b58670564aae30dc9979d7
yennanliu <f339339@gmail.com>
2018-12-11 15:57:51 +0000

    clean code

--- spark_learning/Uber_pairRDD_filter_latlon.java
+++ spark_learning/Uber_pairRDD_filter_latlon.java
@@ -25,13 +25,9 @@ public class Uber_pairRDD_filter_latlon {
 
         JavaPairRDD<String, Float> uberTimeLonPairRDD = uberRDD.mapToPair(getUberTripTimeLon());
 
-        //JavaPairRDD<String, String> airportID_airportLat_PairRDD = airportsRDD.mapToPair(getAirportIDAndLat());
-
         JavaPairRDD<String, Float> uberfilterTimeLonPairRDD = uberTimeLonPairRDD.filter(keyValue -> keyValue._2() > -74);
 
         JavaPairRDD<String, Float> uberfilterTimeLonPairRDD2 = uberTimeLonPairRDD.filter(keyValue -> (keyValue._2() > -73.80) & (keyValue._2() < -73.69999999));
-
-        //JavaPairRDD<String, Float> uberfilterTimeLonPairRDD = uberTimeLonPairRDD.filter(keyValue -> keyValue._2().getValue() > -74);
 
         System.out.println("*** uberTimeLonPairRDD :\n" + uberTimeLonPairRDD.take(30) );
 
