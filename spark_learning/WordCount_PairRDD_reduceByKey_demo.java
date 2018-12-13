e0819a1ee5beb15977e278701edd2db251f2aab2
yennanliu <f339339@gmail.com>
2018-12-13 12:36:00 +0000

    add RDD map demo as comparison

--- spark_learning/WordCount_PairRDD_reduceByKey_demo.java
+++ spark_learning/WordCount_PairRDD_reduceByKey_demo.java
@@ -23,15 +23,34 @@ public class WordCount_PairRDD_reduceByKey_demo {
         JavaRDD<String> lines = sc.textFile("data/word_count.text");
         JavaRDD<String> wordRdd = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
+
+        JavaRDD <String> wordRddMap_0 = lines.map( line -> line.split(" ")[0]);
+
+        //JavaRDD <String> wordRddMap = lines.map( line -> line.split(".")[0]);
+
         JavaPairRDD<String, Integer> wordPairRdd = wordRdd.mapToPair((PairFunction<String, String, Integer>) word -> new Tuple2<>(word, 1));
 
         JavaPairRDD<String, Integer> wordCounts = wordPairRdd.reduceByKey((Function2<Integer, Integer, Integer>) (x, y) -> x + y);
 
         Map<String, Integer> worldCountsMap = wordCounts.collectAsMap();
 
+        System.out.println("lines: " + lines.collect());
+
+        System.out.println("wordRdd: " + wordRdd.collect());
+
+        System.out.println("wordPairRdd: " + wordPairRdd.collect());
+
+        System.out.println("wordCounts: " + wordCounts.collect());
+
+        //System.out.println("worldCountsMap: " + worldCountsMap.collect());
+
+        System.out.println("for loop print wordCounts output: ");
         for (Map.Entry<String, Integer> wordCountPair : worldCountsMap.entrySet()) {
             System.out.println(wordCountPair.getKey() + " : " + wordCountPair.getValue());
-
         }
+
+        System.out.println("wordRddMap_0: " + wordRddMap_0.collect());
+
+        //System.out.println("wordRddMap: " + wordRddMap.collect());
     }
 }
