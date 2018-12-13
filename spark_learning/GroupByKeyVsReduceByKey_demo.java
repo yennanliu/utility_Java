ca992677447c7f1cb66118e9ee75705e21bac05c
yennanliu <f339339@gmail.com>
2018-12-13 11:29:18 +0000

    add new demo array and pairRDD reducebyjey product op demo

--- spark_learning/GroupByKeyVsReduceByKey_demo.java
+++ spark_learning/GroupByKeyVsReduceByKey_demo.java
@@ -18,9 +18,18 @@ public class GroupByKeyVsReduceByKey_demo {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         List<String> words = Arrays.asList("one", "two", "two", "three", "three", "three");
+
         JavaPairRDD<String, Integer> wordsPairRdd = sc.parallelize(words).mapToPair(word -> new Tuple2<>(word, 1));
 
+        JavaPairRDD<String, String> wordsPairRdd2 = sc.parallelize(words).mapToPair(word -> new Tuple2<>(word, "123"));
+
+        JavaPairRDD<String, Integer> wordsPairRdd3 = sc.parallelize(words).mapToPair(word -> new Tuple2<>(word, 100));
+
         System.out.println("wordsPairRdd: " + wordsPairRdd.collect());
+
+        System.out.println("wordsPairRdd2: " + wordsPairRdd2.collect());
+
+        System.out.println("wordsPairRdd3: " + wordsPairRdd3.collect());
 
         List<Tuple2<String, Integer>> wordCountsWithReduceByKey = wordsPairRdd.reduceByKey((x, y) -> x + y).collect();
         System.out.println("wordCountsWithReduceByKey: " + wordCountsWithReduceByKey);
@@ -28,6 +37,9 @@ public class GroupByKeyVsReduceByKey_demo {
         List<Tuple2<String, Integer>> wordCountsWithGroupByKey = wordsPairRdd.groupByKey()
                 .mapValues(intIterable -> Iterables.size(intIterable)).collect();
         System.out.println("wordCountsWithGroupByKey: " + wordCountsWithGroupByKey);
+
+        List<Tuple2<String, Integer>> wordProductWithReduceByKey3 = wordsPairRdd3.reduceByKey((x, y) -> x * y).collect();
+        System.out.println("wordProductWithReduceByKey3: " + wordProductWithReduceByKey3);
     }
 }
 
