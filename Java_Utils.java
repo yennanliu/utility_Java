0ecb2391f9c6881acd2f236aa8c04666ff6dd83e
yennanliue <f339339@gmail.com>
2018-12-23 14:01:23 +0000

    add repo level Java_Utils help script

---
+++ Java_Utils.java
@@ -0,0 +1,8 @@
+public class Java_Utils {
+
+    private Utils(){
+    };
+
+    // a regular expression which matches commas but not commas within double quotations
+    public static final String COMMA_DELIMITER = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+}
