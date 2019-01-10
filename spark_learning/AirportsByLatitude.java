53866925f20fad90af1f27958bf357de69525e4a
yennanliue <f339339@gmail.com>
2019-01-10 08:56:29 +0000

    mute log print results

--- spark_learning/AirportsByLatitude.java
+++ spark_learning/AirportsByLatitude.java
@@ -3,11 +3,15 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class AirportsByLatitude {
 
  public static void main(String[] args) throws Exception {
 
+
+  Logger.getLogger("org").setLevel(Level.ERROR);
 
   SparkConf conf = new SparkConf().setAppName("airportsbylatitude").setMaster("local[2]");
 
@@ -23,7 +27,9 @@ public class AirportsByLatitude {
      splits[1], splits[6] }, ",");
               }
         );
-  airports_name_and_latitude.saveAsTextFile("out/airports_name_and_latitude.text");
+
+  System.out.println(airports_name_and_latitude.take(30));
+  //airports_name_and_latitude.saveAsTextFile("out/airports_name_and_latitude.text");
 
     }
 }
