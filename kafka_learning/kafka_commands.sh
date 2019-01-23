bbcd561a46dcc0ff4cb0e06317f50102ae8d1dc1
yennanliue <f339339@gmail.com>
2019-01-23 08:52:12 +0000

    add create/list/describe/delete topic commands

--- kafka_learning/kafka_commands.sh
+++ kafka_learning/kafka_commands.sh
@@ -14,4 +14,22 @@ kafka-topics
 kafka-topics  --zookeeper  127.0.0.1:2181 --topic first_topic --create  --partitions 3 --replication-factor 1
 
 # 3) list the infromation inside topic 
-first_topic
+kafka-topics  --zookeeper  127.0.0.1:2181 --list 
+
+# 3) describe the the topic 
+kafka-topics  --zookeeper  127.0.0.1:2181 --topic first_topic --describe 
+
+# 4) delete the topic 
+kafka-topics  --zookeeper  127.0.0.1:2181 --topic second_topic --create  --partitions 6 --replication-factor 1
+kafka-topics  --zookeeper  127.0.0.1:2181 --list 
+kafka-topics  --zookeeper  127.0.0.1:2181 --topic second_topic --delete   # just mark the topic for deletion 
+
+
+
+
+
+
+
+
+
+
