06c69db905cb95ed8af1f8264b372770987bcbbf
yennanliue <f339339@gmail.com>
2019-01-23 09:56:22 +0000

    add remove kafka log ref

--- kafka_learning/kill_kafka.sh
+++ kafka_learning/kill_kafka.sh
@@ -5,10 +5,13 @@
 #  KILL RUNNING KAFKA 
 #################################################################
 
-
+# STEP 1) KILL KAFKA 
 PIDS="`ps -ef | grep kafka | egrep "kafka"  | awk '{print $2}'`"
 echo kafka PIDS: $PIDS
 
 for pid in ${PIDS}; 
 	do { echo "Killing $pid"; kill -7 $pid; }; 
 done;
+
+# STEP 2) REMOVE kafka-logs/.lock
+# https://stackoverflow.com/questions/37294996/apache-kafka-failed-to-acquire-lock-on-file-lock-in-tmp-kafka-logs
