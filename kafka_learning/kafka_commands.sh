c99229302c7e1f2a0610a8899d049cf04da477b3
yennanliue <f339339@gmail.com>
2019-01-23 08:44:04 +0000

    add kafka quick start commands :  start/list a new topic

---
+++ kafka_learning/kafka_commands.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+#################################################################
+#  KAFKA CLI COMMANDS   
+#################################################################
+
+
+# 1) kafka-topics: show all commands as list doc.   
+kafka-topics 
+
+# 2) # run kafka-topics ref on zoopkeer (its port)
+# the number of replication-factor should always less then number of broker 
+kafka-topics  --zookeeper  127.0.0.1:2181 --topic first_topic --create  --partitions 3 --replication-factor 1
+
+# 3) list the infromation inside topic 
+first_topic
