9bac4045d61addec455d5e04575f1ee8e00fa4cb
yennanliue <f339339@gmail.com>
2019-01-28 09:16:11 +0000

    add set up kafka consumer command (read from beginning)

--- kafka_learning/kafka_commands.sh
+++ kafka_learning/kafka_commands.sh
@@ -32,8 +32,11 @@ kafka-console-producer  --broker-list  127.0.0.1:9092 --topic new_topic
 # 6) set up producer property 
 kafka-console-producer  --broker-list  127.0.0.1:9092 --topic first_topic --producer-property acks=all  
 
-# 7) set up cosumer  (NOW, OPEN THE OTHER TERMIANL AND RUN THE BELOW COMMAND )
+# 7) set up cosumer (read from now, i.e. only read the producer msg when this consumer is set up ) (NOW, OPEN THE OTHER TERMIANL AND RUN THE BELOW COMMAND )
 kafka-console-consumer   --bootstrap-server  127.0.0.1:9092 --topic first_topic 
+
+# 7) set up cosumer (read from beginning, i.e. read all msg) (NOW, OPEN THE OTHER TERMIANL AND RUN THE BELOW COMMAND )
+kafka-console-consumer   --bootstrap-server  127.0.0.1:9092 --topic first_topic  --from-beginning 
 
 
 
