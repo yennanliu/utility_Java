1f18cd9ae306889091f5e746ecf40768c40f3d32
yennanliue <f339339@gmail.com>
2019-02-02 14:47:48 +0000

    flush data and close producer when process completed

--- kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemo.java
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemo.java
@@ -13,7 +13,6 @@ public class ProducerDemo {
 
         // create producer properties
         // https://kafka.apache.org/documentation/#producerconfigs
-
         String bootstrapServers =  "127.0.0.1:9002";
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -21,16 +20,19 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create the producer
-
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
         // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "HELLO WORLD!");
 
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world!");
-
-        // send data
-
+        // send data - asynchronous
         producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
 
     }
 }
