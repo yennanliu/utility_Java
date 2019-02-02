971f329ebcf4e1a8b708d187f1e71f8c2c311ccc
yennanliue <f339339@gmail.com>
2019-02-02 15:44:10 +0000

    add kafka producer with callback logic

---
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,60 @@
+package com.github.simplesteph.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args) {
+        System.out.print("HELLO WORLD!");
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+        // create producer properties
+        // https://kafka.apache.org/documentation/#producerconfigs
+        String bootstrapServers = "127.0.0.1:9002";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+
+            // create a producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "HELLO WORLD!" + Integer.toString(i));
+
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // execute every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record is successfully sent
+                        logger.info("received new metadata. \n" +
+                                "Topic :" + recordMetadata.topic() + "\n" +
+                                "Partition :" + recordMetadata.partition() + "\n" +
+                                "Offset :" + recordMetadata.offset() + "\n" +
+                                "Timestamp : " + recordMetadata.timestamp());
+
+                    } else {
+                        logger.error("error when producing", e);
+                    }
+                }
+            });
+
+        }
+
+
+        // flush data
+            producer.flush();
+
+            // flush and close producer
+            producer.close();
+
+    }
+}
