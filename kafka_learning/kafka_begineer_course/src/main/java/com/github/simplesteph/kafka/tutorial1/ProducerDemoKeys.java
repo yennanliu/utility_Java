88d84a3c99db3405a4591170491f51386137e027
yennanliue <f339339@gmail.com>
2019-02-06 07:29:52 +0000

    fix bugs in ProducerDemoKeys.java to make it run

--- kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemoKeys.java
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemoKeys.java
@@ -1,5 +1,6 @@
 package com.github.simplesteph.kafka.tutorial1;
 
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -9,14 +10,14 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class ProducerDemoKeys {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        System.out.print("HELLO WORLD!");
 
         final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
 
-        // create producer properties
-        // https://kafka.apache.org/documentation/#producerconfigs
-        String bootstrapServers = "127.0.0.1:9002";
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -25,44 +26,52 @@ public class ProducerDemoKeys {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        for (int i = 0; i < 10; i++) {
 
-
-            String topic = "first topic";
-            String value = "hello world  " +  Integer.toString(i);
-            String key = "id_" +  Integer.toString(i);
-
+        for (int i=0; i<10; i++ ) {
             // create a producer record
-            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
 
-            logger.info("key: + key  "); // log the key
+            String topic = "first_topic";
+            String value = "hello world " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+            // id_0 is going to partition 1
+            // id_1 partition 0
+            // id_2 partition 2
+            // id_3 partition 0
+            // id_4 partition 2
+            // id_5 partition 2
+            // id_6 partition 0
+            // id_7 partition 2
+            // id_8 partition 1
+            // id_9 partition 2
+
 
             // send data - asynchronous
             producer.send(record, new Callback() {
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // execute every time a record is successfully sent or an exception is thrown
+                    // executes every time a record is successfully sent or an exception is thrown
                     if (e == null) {
-                        // the record is successfully sent
-                        logger.info("received new metadata. \n" +
-                                "Topic :" + recordMetadata.topic() + "\n" +
-                                "Partition :" + recordMetadata.partition() + "\n" +
-                                "Offset :" + recordMetadata.offset() + "\n" +
-                                "Timestamp : " + recordMetadata.timestamp());
-
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
                     } else {
-                        logger.error("error when producing", e);
+                        logger.error("Error while producing", e);
                     }
                 }
-            }).get();  // block the .send() to make it synchronous ( not do it in prod)
-
+            }).get(); // block the .send() to make it synchronous - don't do this in production!
         }
 
-
         // flush data
-            producer.flush();
-
-            // flush and close producer
-            producer.close();
+        producer.flush();
+        // flush and close producer
+        producer.close();
 
     }
 }
