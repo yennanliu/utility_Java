1f5b9cf507177f80507b2f5d1699283dc3322f76
yennanliue <f339339@gmail.com>
2019-02-06 07:28:40 +0000

    fix bugs in ProducerDemoWithCallback to make it run

--- kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemoWithCallback.java
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -1,5 +1,6 @@
 package com.github.simplesteph.kafka.tutorial1;
 
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -8,14 +9,14 @@ import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 public class ProducerDemoWithCallback {
+
     public static void main(String[] args) {
-        System.out.print("HELLO WORLD!");
 
         final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
 
-        // create producer properties
-        // https://kafka.apache.org/documentation/#producerconfigs
-        String bootstrapServers = "127.0.0.1:9002";
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -24,37 +25,34 @@ public class ProducerDemoWithCallback {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        for (int i = 0; i < 10; i++) {
 
+        for (int i=0; i<10; i++ ) {
             // create a producer record
-            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "HELLO WORLD!" + Integer.toString(i));
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>("first_topic", "hello world " + Integer.toString(i));
 
             // send data - asynchronous
             producer.send(record, new Callback() {
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // execute every time a record is successfully sent or an exception is thrown
+                    // executes every time a record is successfully sent or an exception is thrown
                     if (e == null) {
-                        // the record is successfully sent
-                        logger.info("received new metadata. \n" +
-                                "Topic :" + recordMetadata.topic() + "\n" +
-                                "Partition :" + recordMetadata.partition() + "\n" +
-                                "Offset :" + recordMetadata.offset() + "\n" +
-                                "Timestamp : " + recordMetadata.timestamp());
-
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
                     } else {
-                        logger.error("error when producing", e);
+                        logger.error("Error while producing", e);
                     }
                 }
             });
-
         }
 
-
         // flush data
-            producer.flush();
-
-            // flush and close producer
-            producer.close();
+        producer.flush();
+        // flush and close producer
+        producer.close();
 
     }
 }
