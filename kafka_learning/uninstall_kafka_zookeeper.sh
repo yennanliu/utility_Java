cf0c3866abf06c6a1011f5fdcc0db0be38861537
yennanliue <f339339@gmail.com>
2019-02-08 09:44:03 +0000

    add command uninstall kafka and zoopkeeper

---
+++ kafka_learning/uninstall_kafka_zookeeper.sh
@@ -0,0 +1,12 @@
+ #!/bin/sh
+
+
+#################################################################
+#  BREW UNINSTALL  KAFKA  / ZOOKEEPER 
+#################################################################
+
+
+brew uninstall kaka 
+brew uninstall ZOOKEEPER 
+brew remove  kaka 
+brew remove ZOOKEEPER 
