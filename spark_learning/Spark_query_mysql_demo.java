59a84ce4f00450f030d0f1854075ea4496b6f6a9
yennanliue <f339339@gmail.com>
2019-02-08 14:13:32 +0000

    fix mysql connect error and make this script run

--- spark_learning/Spark_query_mysql_demo.java
+++ spark_learning/Spark_query_mysql_demo.java
@@ -17,36 +17,25 @@ public class Spark_query_mysql_demo {
 
         Logger.getLogger("org").setLevel(Level.ERROR);
 
-
         SparkConf conf = new SparkConf().setAppName("StackOverFlowSurvey").setMaster("local[1]");
-        
-        SparkContext sparkContext = new SparkContext(conf);
-        
-        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkContext);
-
+         
         SparkContext sc = new SparkContext("local", "spark-mysql-test", conf);
         
         SQLContext sqlContext = new SQLContext(sc);
 
-
         // here you can run sql query
         String sql = "(select * from Soft_Job LIMIT 1000) as test_table";
-        // or use an existed table directly
-        // String sql = "table1";
 
-        // DataFrame jdbcDF = sqlContext.read().format("jdbc"). options(options).load();
-        // https://stackoverflow.com/questions/27718382/how-to-work-with-mysql-and-apache-spark
+        String url = "jdbc:mysql://103.29.68.107:3306/PTTData?user=guest&password=123";
 
-        DataFrame dataFrame = sqlContext
-            .read()
-            .format("jdbc")
-            .options("url", "jdbc:mysql://103.29.68.107/PTTData?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
-            .options("user", <user>)
-            .options("password", <password>)
-            .options("dbtable", sql)
-            .load();
-
-        dataFrame.show(30); 
+        Dataset <Row> df = sqlContext
+                      .read()
+                      .format("jdbc")
+                      .option("url", url)
+                      .option("dbtable", sql)
+                      .load();
+        df.show(30); 
 
     }
 }
+
