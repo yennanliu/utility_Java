beec28e6e49ed2710f465f7e5bfc544b7688322b
yennanliue <f339339@gmail.com>
2019-02-20 07:15:30 +0000

    make 1st java twitter stream app work

--- kafka_learning/kafka_begineer_course/src/main/java/com/github/yen/kafka/tutorial2/TwitterProducer.java
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/yen/kafka/tutorial2/TwitterProducer.java
@@ -1,59 +1,105 @@
 package com.github.yen.kafka.tutorial2;
 
 import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.LoggerFactory;
 import sun.jvm.hotspot.opto.Block;
-
+import org.slf4j.Logger;
+import java.awt.*;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class TwitterProducer {
 
-    public  TwitterProducer(){}
-
-    public static void main(String[] args) {
-        new TwitterProducer().run;
-
-    }
-
-
-    public void run(){
-
-
-        // create a twitter client
-
-        // create a kafka producer
-
-        // loop to send tweets to kafka
-
-
-    }
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
 
     String consumerKey = "";
     String consumerSecret = "";
     String token = "";
     String secret = "";
 
-    public void createTwitterProducer(){
-    // setup blocking queues
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+    public  TwitterProducer(){}
+
+    public static void main(String[] args) {
+       new TwitterProducer().run();
+
+    }
+
+
+    public void run(){
+
+        logger.info("set up");
+
+        // setup blocking queues
+        BlockingQueue<String> msgQueue =  new LinkedBlockingQueue<String>(1000);
+
+        // create a twitter client
+        Client client = createTwitterProducer(msgQueue);
+
+        // establish a connection
+        client.connect();
+
+        // create a kafka producer
+
+        // loop to send tweets to kafka
+        // on a different thread, or multiple different threads....
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null){
+
+                logger.info(msg);
+            }
+        }
+
+        logger.info("end of application");
+
+    }
+
+
+
+    public Client createTwitterProducer(BlockingQueue<String> msgQueue ){
 
         // https://github.com/twitter/hbc
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-        List<String> terms = Lists.newArrayList("kafka");
+        List<String> terms = Lists.newArrayList("bitcoin");
         hosebirdEndpoint.trackTerms(terms);
 
         // These secrets should be read from a config file
-        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        // create a client
+
+        ClientBuilder builder = new ClientBuilder()
+                    .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                    .hosts(hosebirdHosts)
+                    .authentication(hosebirdAuth)
+                    .endpoint(hosebirdEndpoint)
+                    .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
+
 
     }
 
