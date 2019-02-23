683833c05ee28d118367cb923d6541252fa44676
yennanliue <f339339@gmail.com>
2019-02-23 11:00:09 +0000

    add code setup kafka producer and send back twitter data to consumer

--- kafka_learning/kafka_begineer_course/src/main/java/com/github/yen/kafka/tutorial2/TwitterProducer.java
+++ kafka_learning/kafka_begineer_course/src/main/java/com/github/yen/kafka/tutorial2/TwitterProducer.java
@@ -10,11 +10,15 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.LoggerFactory;
 import sun.jvm.hotspot.opto.Block;
 import org.slf4j.Logger;
 import java.awt.*;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -53,6 +57,11 @@ public class TwitterProducer {
 
         // create a kafka producer
 
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+
+
+
         // loop to send tweets to kafka
         // on a different thread, or multiple different threads....
         while (!client.isDone()) {
@@ -66,6 +75,14 @@ public class TwitterProducer {
             if (msg != null){
 
                 logger.info(msg);
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                     if (e != null){
+                         logger.error("sth error happened",e);
+                        }
+                    }
+                });
             }
         }
 
@@ -100,6 +117,22 @@ public class TwitterProducer {
         Client hosebirdClient = builder.build();
         return hosebirdClient;
 
+
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer(){
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        return producer;
 
     }
 
