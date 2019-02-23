e2361276b130df2a9e730052e083f1fcd4f6352b
yennanliue <f339339@gmail.com>
2019-02-23 10:59:04 +0000

    add launch consumer command

--- kafka_learning/kafka_commands.sh
+++ kafka_learning/kafka_commands.sh
@@ -66,6 +66,8 @@ kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic first_topic --f
 # 1) create twitter topic  
 kafka-topics --zookeeper 127.0.0.1:2181 --create --topic twitter_tweets --partitions 6 --replication-factor 1 
 
+# 2) launch a consumer 
+kafka-console-consumer   --bootstrap-server  127.0.0.1:9092 --topic twitter_tweets  
 
 
 
