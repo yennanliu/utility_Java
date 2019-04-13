c13e42d1b866f50789a74f08db83eed2d96ae864
yennanliue <f339339@gmail.com>
2019-04-13 09:43:46 +0100

    fix code layout

--- archive/currentdate.java
+++ archive/currentdate.java
@@ -4,12 +4,11 @@ public class currentdate {
    public static void main(String args[]) {
       // Instantiate a Date object
       Date date = new Date();
-
       // display time and date
       String str = String.format("Current Date/Time : %tc", date );
       System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
       System.out.printf("%n");
-	  System.out.printf(str);
-	  System.out.printf("%n");
+	   System.out.printf(str);
+	   System.out.printf("%n");
    }
 }
