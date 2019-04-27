e7189041f1e2baf4ceae640c014a6256317dd920
yennanliue <f339339@gmail.com>
2019-04-27 16:41:11 +0100

    modify Dockerfile command make it be able to run both  Sample.java and ForLoopExample.java scripts

--- java-docker-application-demo/Sample.java
+++ java-docker-application-demo/Sample.java
@@ -1,5 +1,5 @@
-class Sample{ 
+class Sample { 
    public static void main(String[] args)  { 
    System.out.println("This is java application by using docker ");
-   } 
-   }
+   			 								} 
+   			 }
