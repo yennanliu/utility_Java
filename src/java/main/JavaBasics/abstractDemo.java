0232863c714e4b21f0659b58f91271deca3c957a
yennanliu <f339339@gmail.com>
2020-08-29 12:02:54 +0800

    add more examples (abstractDemo.class

--- src/java/main/JavaBasics/abstractDemo.java
+++ src/java/main/JavaBasics/abstractDemo.java
@@ -1,3 +1,4 @@
+// example 1
 abstract class Animal {
     // define abstract method : shout 
     public abstract void shout(); 
@@ -12,11 +13,33 @@ class Dog extends Animal {
 
 }
 
+// example 2 
+abstract class Car {
+    public abstract void start();
+    public abstract void stop();
+}
+
+class Benz extends Car {
+    // need to implement ALL abstract methods in abstract class
+    public void start(){
+        System.out.println("car start ....");
+    }
+
+    public void stop(){
+        System.out.println("car stop ....");
+    }
+}
+
 public class abstractDemo {
     public static void main(String[] args){
-        // run
+        // run 1 
         Dog dog = new Dog();
         dog.shout();
+
+        // run 2
+        Benz benz = new Benz();
+        benz.start();
+        benz.stop(); 
     }
 
 }
