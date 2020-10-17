0cec925a91b1d95ef0a781813d1a57937d599b2f
yennanliu <f339339@gmail.com>
2020-10-18 00:22:52 +0800

    print process, fix code layout

--- src/java/main/JavaBasics/DeleteDirectoryFiles.java
+++ src/java/main/JavaBasics/DeleteDirectoryFiles.java
@@ -4,31 +4,29 @@ public class DeleteDirectoryFiles{
 
     public static void main (String[] args){
 
-    // the directory need to delte
-    File files = new File("test"); 
+        // the directory need to delte
+        File files = new File("test"); 
 
-    // run the delete method
-    deleteDir(files);
+        // run the delete method
+        deleteDir(files);
     } 
 
     // delte method
     public static void deleteDir(File files){
-    // get the lists of file under files and save it in array
-    File[] listFiles = files.listFiles();
+        // get the lists of file under files and save it in array
+        File[] listFiles = files.listFiles();
 
-    // go through array
-    for (File file: listFiles){
-        // if is directory, then run the delete method recursively
-        if (file.isDirectory()){
-            deleteDir(file);
-        }
-
-        // if is file, delete it directly
-        file.delete();
-     }
+        // go through array
+        for (File file: listFiles){
+            System.out.println("file name : " + file);
+            // if is directory, then run the delete method recursively
+            if (file.isDirectory()){
+                deleteDir(file);
+            }
+            // if is file, delete it directly
+            file.delete();
+         }
         // after deleting all files under directory, delete the directory as final step
         files.delete();
-
-  }
-
+      }
 }
